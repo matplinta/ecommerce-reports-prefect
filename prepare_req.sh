d53a615e24bd2042ae26fa1e1dd721df6296a324
Mateusz Plinta <matplinta@gmail.com>
2026-05-07 12:14:52 +0200

    update versions

--- prepare_req.sh
+++ prepare_req.sh
@@ -2,5 +2,5 @@
 # This script prepares the Python environment by installing dependencies
 apt-get update && apt-get install -y --no-install-recommends build-essential
 PREFECT_VERSION=$(python -c "import prefect; print(prefect.__version__)")
-echo "prefect==${PREFECT_VERSION}" > /tmp/prefect-constraint.txt 
+echo "prefect[email,slack]==${PREFECT_VERSION}" > /tmp/prefect-constraint.txt 
 uv pip compile pyproject.toml --constraints /tmp/prefect-constraint.txt | uv pip sync -
